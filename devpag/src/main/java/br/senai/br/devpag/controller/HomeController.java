8b4c50efb4b8fb887bc467a001104a5b1bbc8772
luizanuness <nunesss.luiza@gmail.com>
2024-08-22 15:43:39 -0300

    att model e resources

---
+++ devpag/src/main/java/br/senai/br/devpag/controller/HomeController.java
@@ -0,0 +1,15 @@
+package br.senai.br.devpag.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+    @GetMapping
+    public String index(){
+        return "home/index";
+    }
+}
