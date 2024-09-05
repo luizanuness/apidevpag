04c1e80a7aae3babdda315a999ef8a1a073cc758
luizanuness <nunesss.luiza@gmail.com>
2024-09-05 10:34:41 -0300

    commit

---
+++ devpag/src/test/java/br/senai/br/devpag/controller/AlunoControllerTest.java
@@ -0,0 +1,17 @@
+package br.senai.br.devpag.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AlunoControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
