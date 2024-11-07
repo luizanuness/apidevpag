d144021dc7dc922726ccde53cd801e6590f58041
luizanuness <nunesss.luiza@gmail.com>
2024-11-07 07:49:50 -0300

    Update

--- devpag/src/main/java/br/senai/br/devpag/repository/AlunosResponsaveisRepository.java
+++
@@ -1,7 +0,0 @@
-package br.senai.br.devpag.repository;
-
-import br.senai.br.devpag.model.AlunosReponsaveis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunosResponsaveisRepository extends JpaRepository<AlunosReponsaveis, Long> {
-}
